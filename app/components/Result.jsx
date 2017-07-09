216786124fc430051bf6a1784e1c147372197ba3
JP Marra <jp.marra@me.com>
2017-07-08 20:45:59 -0400

    adds media queries for mobile

--- app/components/Result.jsx
+++ app/components/Result.jsx
@@ -3,11 +3,11 @@ import Skycons from 'react-skycons';
 
 const Result = (props) => (
   <div className="result-info">
-    <section className="details">
+    <section className="details col-xs-3">
       <header className="header">Your travel time is</header>
       <text className="travel-time">{props.travelTime}</text>
     </section>
-    <section className="details">
+    <section className="details col-xs-3">
       <header className="header">Current weather</header>
       <Skycons className="weather-icon" color="#0065BD" icon={props.weather.icon} autoPlay={true} />
       <text className="weather-body">{props.weather.summary}</text>
